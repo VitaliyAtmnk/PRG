3afabb745231f3a5a1e783610d6312dc82c78e2d
Vitalij Atamanjuk <atamanjuk@infis.cz>
2024-10-21 12:37:44 +0200

    Cviceni - cyklus For

---
+++ 2_Říjen/hodina_7/For.java
@@ -0,0 +1,12 @@
+package hodina_7;
+
+public class For {
+    public static void main(String[] args) {
+
+        /* Vypište všechny násobky pěti  od 1 do 1000 */
+
+        /* Vypište všechny čísla od 100 do 0 */
+
+        /* upravte předchozí příklad tak aby vypisoval 2 mocniny čísel */
+    }
+}
