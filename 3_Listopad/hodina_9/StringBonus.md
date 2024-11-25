74c3d646a8e0770206acbdeafbeb85efd9cbee80
Vitalij Atamanjuk <atamanjuk@infis.cz>
2024-11-25 11:58:27 +0100

    Řetězce

---
+++ 3_Listopad/hodina_9/StringBonus.md
@@ -0,0 +1,21 @@
+# Bonus
+
+### Validace hesla
+
+- Načtěte od uživatele heslo a zkontrolujte, zda vyhovuje požadavkům
+    - Alespoň 12 znaků
+    - Žádné bíle znaky na **konci**, nebo **začátku**
+    - obsahuje slovo `red` (v jakékoliv podobě (Red, rEd, atd.))
+    - neobsahuje slovo `vacek` (v jakékoliv podobě)
+    - končí hvězdičkou
+
+### Převod soustavy
+
+- Načtěte od uživatele řetězec reprezentující číslo v dvojkové soustavě.
+- Převeďte na desítkovou soustavu
+
+### Počet slov
+
+- Spočítejte počet slov v textu.
+- Slova nemusí doopravdy existovat, takže např.:
+  - adsfasdf Dobré ránoooo `počet slov = 3`
---
+++ 3_Listopad/hodina_9/Strings.java
@@ -0,0 +1,96 @@
+package hodina_9;
+
+public class Strings {
+    public static void main(String[] args) {
+        {
+            String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce augue ligula, euismod sit amet rutrum ac, maximus nec dolor. Nullam a diam erat. Fusce elementum augue in ipsum maximus consequat. Cras rhoncus ut mauris non dignissim. Nunc tincidunt feugiat nunc, finibus dignissim lacus bibendum in. Pellentesque dapibus venenatis dui. Donec vulputate semper arcu, a pharetra arcu volutpat sed. Sed at tellus quis lorem aliquam feugiat. Morbi lorem diam, scelerisque vitae tempor in, viverra vitae quam. Sed iaculis enim quis orci varius placerat. Nullam feugiat porta dui, at vulputate lacus sodales sed. Fusce rhoncus, libero at interdum pellentesque, quam turpis pulvinar nisl, id condimentum ex justo quis mauris.\n" +
+                    "Suspendisse venenatis nec leo ut eleifend. Morbi varius dignissim lectus sed facilisis. Proin sollicitudin nunc quis ante egestas sagittis. Ut imperdiet sodales quam, et fringilla orci cursus eu. Phasellus consequat aliquet arcu, vitae accumsan nisl ullamcorper ac. Sed non lacus tristique, accumsan mi eu, sollicitudin tellus. Vestibulum euismod dui quis nisi auctor lobortis. Aliquam efficitur faucibus mauris at tempor.\n" +
+                    "Aenean elementum eget urna nec vulputate. Suspendisse imperdiet quam nisi, et elementum quam interdum commodo. Morbi ut neque ut ex tempor tempus lacinia ac metus. Nulla in lorem et neque tincidunt elementum. Maecenas felis arcu, suscipit a mollis non, volutpat ac arcu. Curabitur justo eros, placerat vulputate cursus et, ultrices sit amet nisi. Sed massa ligula, pretium vel ornare nec, fermentum at sapien. Mauris laoreet, justo vel congue laoreet, elit tellus convallis quam, a consectetur velit urna feugiat dolor. Quisque semper turpis nunc, vel feugiat leo euismod eget. Mauris enim orci, semper non sodales et, porta ac neque.\n" +
+                    "Vivamus sed condimentum purus. Aenean in dictum metus. Donec et mattis ex, ut suscipit ipsum. Ut lobortis ligula nec orci faucibus vulputate. Mauris non faucibus ligula, sit amet rutrum eros. Etiam eget purus eros. Proin viverra interdum ante at gravida. Pellentesque eget gravida nulla. Etiam sem ipsum, viverra ac velit eu, aliquet maximus arcu. Nam vestibulum venenatis bibendum. Aliquam erat volutpat.\n" +
+                    "Pellentesque a lacus urna. Aenean tincidunt porttitor sodales. Cras ac iaculis purus. Praesent sit amet ultrices justo. Duis et mauris est. Vestibulum id faucibus tortor. Mauris orci dolor, convallis a massa vitae, dignissim euismod ipsum. Donec pellentesque risus id posuere sodales. Sed semper nisi nibh. Donec ultrices in mauris non euismod. Etiam quis nulla nec lorem feugiat ultricies. Fusce ut libero eleifend, commodo elit ac, condimentum est. Quisque aliquam tellus nec dolor bibendum, non auctor nibh facilisis. Quisque bibendum elit eget commodo bibendum. Aliquam enim ligula, posuere eu odio id, efficitur gravida sapien. Aliquam erat volutpat.\n" +
+                    "Proin sed nibh nisl. Nulla lacinia sed velit eu malesuada. Aenean massa arcu, vestibulum vel malesuada a, gravida nec sapien. Sed gravida odio et eleifend sodales. Integer sed feugiat justo. Sed dapibus fermentum viverra. Quisque iaculis auctor tellus ac mattis.\n" +
+                    "Pellentesque nec placerat leo, eu efficitur dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean at venenatis felis. Donec vitae vehicula diam. Maecenas quis velit consectetur, lacinia dolor efficitur, consequat erat. Vivamus nec nibh mattis, suscipit purus a, suscipit eros. Donec in enim ut eros venenatis fermentum at vel nisl.\n" +
+                    "Ut quam leo, rutrum eget malesuada sit amet, laoreet ac ligula. Nunc aliquam lectus vel risus rhoncus ultricies. Cras scelerisque arcu ut nunc varius pretium. Integer a tincidunt urna. Praesent vitae lorem at nisl accumsan auctor iaculis eget lorem. Cras a placerat quam, non ornare purus. Quisque quis ex nulla. Morbi commodo, massa quis maximus congue, felis libero maximus enim, sodales facilisis turpis augue id est.\n" +
+                    "Morbi at velit fermentum lectus posuere commodo. Mauris commodo diam ut varius tincidunt. Duis erat metus, laoreet sollicitudin scelerisque eu, commodo gravida ipsum. Ut imperdiet dapibus arcu, at molestie ante cursus nec. Etiam vel dapibus diam. Ut tristique rutrum vestibulum. Sed elementum justo lorem, at ultrices felis porta at. Ut felis est, accumsan eu nibh at, aliquet consequat mauris. Maecenas a dui efficitur, viverra est eu, imperdiet mi. Cras pellentesque purus eget risus suscipit, ut molestie lacus rutrum. Nulla elementum neque eu augue ultricies, vel malesuada leo maximus. Nunc lobortis nisl at sem faucibus sagittis.\n" +
+                    "Integer et suscipit elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at tellus nec ipsum vulputate hendrerit. Etiam quis lectus et erat dignissim vehicula. Sed ut vestibulum odio. Aenean eu leo sit amet dui auctor imperdiet non ac mauris. Sed ligula ipsum, mattis quis neque a, posuere maximus arcu. Suspendisse sed lacinia orci, non sodales ex. Ut ac cursus nulla. Praesent varius commodo imperdiet. In semper laoreet nunc, ornare ullamcorper tortor finibus non. Vestibulum sollicitudin ante lacus, ac volutpat nisl vestibulum eget. Ut et purus massa. Duis non massa tellus. Curabitur non tincidunt nunc, quis ullamcorper nunc. Mauris congue a nibh nec semper.\n" +
+                    "Vivamus vel eros pellentesque, tincidunt enim at, eleifend lectus. Cras rutrum lobortis dolor a ultrices. Etiam nulla sapien, finibus quis auctor et, laoreet sit amet lorem. Nam sit amet lorem non purus commodo pharetra. Vestibulum ullamcorper turpis et ligula pharetra, ut cursus risus placerat. Curabitur semper consectetur mattis. Etiam iaculis mauris ut sagittis pharetra. Mauris porta enim et purus placerat accumsan. Vestibulum viverra est mi, vel cursus turpis volutpat eu. Donec purus libero, maximus nec nulla ac, eleifend ultrices odio. Duis egestas et massa a dapibus. Cras laoreet enim non nisl auctor dignissim. Donec at dignissim velit, quis feugiat erat. Morbi commodo massa et lacus vestibulum, a posuere erat elementum.\n" +
+                    "Nulla magna mi, consequat et augue nec, fermentum sodales leo. Praesent vel porta lacus. Nulla facilisi. In sit amet nisi non elit hendrerit facilisis eget molestie libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis et urna diam. Phasellus interdum ante pellentesque massa aliquam, in maximus quam elementum. Fusce vestibulum venenatis purus nec tincidunt. Sed elementum quis erat id suscipit.\n" +
+                    "Nullam sit amet consectetur felis. Proin luctus elementum purus, ut aliquet magna vestibulum vel. Nulla facilisi. Curabitur posuere diam consectetur, ultrices augue et, faucibus tortor. Vivamus ac justo hendrerit, vulputate risus eget, suscipit orci. Vivamus hendrerit purus sit amet nunc commodo.";
+//            System.out.println(text);
+        }
+
+        /*
+            length();
+            charAt(int index);
+            indexOf(String s);
+            toUpperCase();
+            toLowerCase();
+            trim();
+            replace(char, char);
+            substring(int start);
+        */
+
+        String jmeno = "Martins Ferreira Pereira De Magalhaes";
+        String prijmeni = "Mandová";
+        jmeno = jmeno + " " + prijmeni;
+
+        int delkaRetezce = "Ahoj".length();
+//        System.out.println("delka retezce Ahoj: " + delkaRetezce);
+
+        delkaRetezce = jmeno.length();
+//        System.out.println("delka retezce " + jmeno + ": " + delkaRetezce);
+
+        int indexM = jmeno.indexOf('M');
+//        System.out.println("Index \"M\": " + indexM);
+
+        int indexMalehoE = jmeno.indexOf('e');
+//        System.out.println("Index \"e\": " + indexMalehoE);
+
+        int indexAcka = "Abba".indexOf('A');
+//        System.out.println("Index \"A\": " +indexAcka);
+
+        int indexMalehoBecka = "Abba".indexOf('b');
+//        System.out.println("Index \"b\": " + indexMalehoBecka);
+
+//        System.out.println(jmeno.toUpperCase());
+//        System.out.println(jmeno.toLowerCase());
+
+//        System.out.println("aHoJ".toUpperCase());
+//        System.out.println("aHoJ".toLowerCase());
+
+
+        String password = "      heslo          ";
+//        System.out.println(password.trim());
+//        System.out.println("void                 ".strip());
+
+        String vdanaMandova = jmeno.replace("Mandová", "Cervenokostelecká");
+//        System.out.println(vdanaMandova);
+
+        String oprava = vdanaMandova.replace('C', 'Č');
+//        System.out.println(oprava);
+
+        // Java a porovnávání řetězců
+        {
+            // equals porovnává hodnoty
+            boolean case1 = new String("test").equals("test"); // true
+
+            // new String("test") a "test" jsou rozdílné objekty. == porovnává, jestli jsou to stejný objekty
+            boolean case2 = new String("test") == "test"; // false
+
+            // stejně jako case2
+            boolean case3 = new String("test") == new String("test"); // false
+
+            // jedná se o tzv. "literály" a to jsou vždy stejné objekty
+            boolean case4 = "test" == "test"; // true
+
+            // literály "te" a "st" se spojí v jeden a odkazuje se poté na jeden a ten samý objekt
+            boolean case5 = "test" == "te" + "st"; // true
+
+            // System.out.printf("1. %s\n2. %s\n3. %s\n4. %s\n5. %s\n", case1, case2, case3, case4, case5);
+        }
+
+        // pro jednoduchost používejte u řetězců místo ==, equals()
+//        System.out.println("Bob".equals("bob"));
+//        System.out.println("Bob".equalsIgnoreCase("bob"));
+    }
+}
